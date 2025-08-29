f40683c24e740b95d8853a7b2804cf8635798a0b
swlee <swo98@naver.com>
2025-08-29 20:33:09 +0900

    add: zset 사용을 위한 template config 빈 등록

--- src/main/java/lab/redisprac/config/redis/RedisTemplateConfig.java
+++ src/main/java/lab/redisprac/config/redis/RedisTemplateConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -24,5 +25,10 @@ public class RedisTemplateConfig {
         template.setValueSerializer(serializer);
         template.setHashValueSerializer(serializer);
         return template;
+    }
+
+    @Bean
+    public ZSetOperations<String, Object> zSetOperations(RedisTemplate<String, Object> redisTemplate) {
+        return redisTemplate.opsForZSet();
     }
 }
